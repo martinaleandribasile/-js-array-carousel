bfc8f92ea098de7dac7a9e6ac4ef41f710b11004
MartinaBasile <martinaleandribasile@gmail.com>
2022-08-29 17:01:55 +0200

    add effect on sideimg

--- script.js
+++ script.js
@@ -5,14 +5,6 @@ const sideImgBox = document.getElementById("sideimgbox");
 const mainImages = ["01", "02", "03", "04", "05"];
 let starterimgNumber = 0;
 let sideimgclass = "sideimg"
-/*for (let i = 0; i < mainImages.length; i++) {
-    const sideimg = mainImages[i];
-    let sideimgcreate = document.createElement(`<img src="img/${sideimg}.jpg">`);
-    sideimg.classList.add("sideimg")
-    sideImgBox.innerHTML += sideimgcreate
-};*/
-
-
 for (let i = 0; i < mainImages.length; i++) {
     const image = mainImages[i];
     const imageClass = i === starterimgNumber ? 'd-block mainimg' : 'd-none mainimg';
@@ -22,38 +14,48 @@ for (let i = 0; i < mainImages.length; i++) {
     const sideimage = mainImages[i];
     sideImgBox.innerHTML += `<img src="img/${sideimage}.jpg" class="${sideimgclass}">`
 };
+
+
 const previousButton = document.getElementById('previous');
 const nextButton = document.getElementById('next');
 let activeIndex = 0
+const sideimgarray = document.querySelectorAll(".sideimg");
+sideimgarray[0].classList.add("sideonfocus");
+console.log(sideimgarray);
 
 nextButton.addEventListener('click', function () {
     if (activeIndex < mainImages.length - 1) {
         boxMainImg.innerHTML = '';
         activeIndex++;
-        /*for (let i = 0; i < mainImages.length; i++) {
-            if (i === activeIndex) {
-                sideimgclass = "sideimg sideonfocus";
-            } else {
-                sideimgclass = "sideimg"
-            }
-        }*/
-        for (let i = 0; i < mainImages.length; i++) {
-            const image = mainImages[i];
-            const imageClass = i === activeIndex ? 'd-block mainimg' : 'd-none mainimg'
-            boxMainImg.innerHTML += '<img class="' + imageClass + '" src="img/' + image + '.jpg" alt="due" />'
-        }
-    }
-});
-previousButton.addEventListener('click', function () {
-    if (activeIndex > 0) {
-        boxMainImg.innerHTML = '';
-        activeIndex--;
 
         for (let i = 0; i < mainImages.length; i++) {
             const image = mainImages[i];
             const imageClass = i === activeIndex ? 'd-block mainimg' : 'd-none mainimg'
             boxMainImg.innerHTML += '<img class="' + imageClass + '" src="img/' + image + '.jpg" alt="due" />'
+            if (i === activeIndex) {
+                sideimgarray[i].classList.add("sideonfocus")
+            } else {
+                sideimgarray[i].classList.remove("sideonfocus")
+            }
+        }
+    }
+
+});
+previousButton.addEventListener('click', function () {
+    if (activeIndex > 0) {
+        boxMainImg.innerHTML = '';
+        activeIndex--;
+        for (let i = 0; i < mainImages.length; i++) {
+            const image = mainImages[i];
+            const imageClass = i === activeIndex ? 'd-block mainimg' : 'd-none mainimg'
+            boxMainImg.innerHTML += '<img class="' + imageClass + '" src="img/' + image + '.jpg" alt="due" />'
+            if (i === activeIndex) {
+                sideimgarray[i].classList.add("sideonfocus")
+            } else {
+                sideimgarray[i].classList.remove("sideonfocus")
+            }
         }
     }
 });
+
 
