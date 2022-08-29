825f994fa3603fc8a7b7b6e082c448efbb9fed91
MartinaBasile <martinaleandribasile@gmail.com>
2022-08-29 21:09:11 +0200

    add comments

--- script.js
+++ script.js
@@ -1,12 +1,21 @@
 console.log("Js ok");
-
+//prendo gli elementi necessari
 const boxMainImg = document.getElementById("mainimgbox");
 const sideImgBox = document.getElementById("sideimgbox");
-const mainImages = ["01", "02", "03", "04", "05"];
-let starterimgNumber = 0;
+const previousButton = document.getElementById('previous');
+const nextButton = document.getElementById('next');
 let sideimgclass = "sideimg"
+
+//creo il mio array 
+const mainImages = ["01", "02", "03", "04", "05"];
+
+//inizializzo un starternumber 0 per compararlo nel if
+let starterimgNumber = 0;
+
+// creo dei loop per creare i tag immagine sia nel main che nel said box
 for (let i = 0; i < mainImages.length; i++) {
     const image = mainImages[i];
+    //uso il mio startnuber 0 per condizionare le classi presenti nelle immagini nel mainbox
     const imageClass = i === starterimgNumber ? 'd-block mainimg' : 'd-none mainimg';
     boxMainImg.innerHTML += `<img src="img/${image}.jpg" class="${imageClass}">`
 };
@@ -15,16 +24,16 @@ for (let i = 0; i < mainImages.length; i++) {
     sideImgBox.innerHTML += `<img src="img/${sideimage}.jpg" class="${sideimgclass}">`
 };
 
-
-const previousButton = document.getElementById('previous');
-const nextButton = document.getElementById('next');
-let activeIndex = 1
+//prendo in un array tutte le immagini side create nel for precedente
 const sideimgarray = document.querySelectorAll(".sideimg");
+//all immagine a indice 0 do gia' la classe focus
 sideimgarray[0].classList.add("sideonfocus");
 console.log(sideimgarray);
 
+//inizializzo un indice a 1 in quanto al primo loop voglio che passi direttamente all indice 1
+let activeIndex = 1
+// creo evento click sul nextbutton
 nextButton.addEventListener('click', function () {
-
     console.log("activenext", activeIndex)
     if (activeIndex < mainImages.length - 1) {
         boxMainImg.innerHTML = '';
@@ -58,10 +67,10 @@ nextButton.addEventListener('click', function () {
 });
 
 previousButton.addEventListener('click', function () {
-    activeIndex--;
+    console.log("parto da ", activeIndex)
+    activeIndex--
     console.log("active", activeIndex)
     if (activeIndex > 0) {
-
         boxMainImg.innerHTML = '';
         console.log("nell if", activeIndex)
         for (let i = 0; i < mainImages.length; i++) {
@@ -74,7 +83,6 @@ previousButton.addEventListener('click', function () {
                 sideimgarray[i].classList.remove("sideonfocus")
             }
         }
-
     } else if (activeIndex === 0) {
         boxMainImg.innerHTML = '';
         for (let i = 0; i < mainImages.length; i++) {
